1aeab6896ac2f453bb855a88b1430a93fc7811bb
Stephen Ficklin <stephen.ficklin@allelesystems.com>
2014-03-05 19:45:33 -0500

    Updated KEGG reports to work without jsTree moduel

--- theme/js/tripal_analysis_kegg.js
+++ theme/js/tripal_analysis_kegg.js
@@ -1,28 +1,61 @@
+(function($) {
+  Drupal.behaviors.tripal_kegg_analysisBehavior = {
+    attach: function (context, settings){ 
+      // code for toggling trees below borrowed from here: http://homework.nwsnet.de/releases/ea21/
+    
+      // Find list items representing folders and
+      // style them accordingly.  Also, turn them
+      // into links that can expand/collapse the
+      // tree leaf.
+      $('#heirarchy_terms_list li > ul').each(function(i) {
+        // Find this list's parent list item.
+        var parent_li = $(this).parent('li');
 
+        // Style the list item as folder.
+        parent_li.addClass('folder');
+
+        // Temporarily remove the list from the
+        // parent list item, wrap the remaining
+        // text in an anchor, then reattach it.
+        var sub_ul = $(this).remove();
+        parent_li.wrapInner('<a/>').find('a').click(function() {
+          // Make the anchor toggle the leaf display.
+          sub_ul.toggle();
+        });
+        parent_li.append(sub_ul);
+      });
+
+      // Hide all lists except the outermost.
+      $('#heirarchy_terms_list ul ul').hide();
+    }
+  };
+})(jQuery);
 
 if (Drupal.jsEnabled) {
    $(document).ready(function() {
+     
 
+  
        // Select default KEGG analysis when available
        var selectbox = $('#edit-tripal-analysis-kegg-select');
        if(selectbox.length > 0){ 
-    	   var option = document.getElementById("analysis_id_for_kegg_report");
-    	   if (option) {
-    		   var options = document.getElementsByTagName('option');
-    		   var index = 0;
-    		   for (index = 0; index < options.length; index ++) {
-    			   if (options[index].value == option.value) {
-    				   break;
-    			   }
-    		   }
-    		   selectbox[0].selectedIndex = index;
-    		   var baseurl = tripal_get_base_url();
-    		   tripal_analysis_kegg_org_report(option.value, baseurl);
-    		// Otherwise, show the first option by default
-    	   } else {
-    		   selectbox[0].selectedIndex = 1;
-    		   selectbox.change();
-    	   }
+         var option = document.getElementById("analysis_id_for_kegg_report");
+         if (option) {
+           var options = document.getElementsByTagName('option');
+           var index = 0;
+           for (index = 0; index < options.length; index ++) {
+             if (options[index].value == option.value) {
+               break;
+             }
+           }
+           selectbox[0].selectedIndex = index;
+           var baseurl = tripal_get_base_url();
+           tripal_analysis_kegg_org_report(option.value, baseurl);
+        // Otherwise, show the first option by default
+         } else {
+           selectbox[0].selectedIndex = 1;
+           selectbox.change();
+         }
        }
 
    });
@@ -72,91 +105,5 @@ if (Drupal.jsEnabled) {
          }
       });
       return false;
-   }
-
-   //------------------------------------------------------------
-   // This function initializes a KEGG term tree
-   function init_kegg_tree(id){
-
-      // Form the link for the following ajax call
-      var theme_path = baseurl + '/' + themedir + "/js/jsTree/source/themes/";
-	   
-      $("#" + id).tree ({
-	    data    : {
-	        type    : "predefined", // ENUM [json, xml_flat, xml_nested, predefined]
-	        method  : "GET",        // HOW TO REQUEST FILES
-	        async   : false,        // BOOL - async loading onopen
-	        async_data : function (NODE) { return { id : $(NODE).attr("id") || 0 } }, // PARAMETERS PASSED TO SERVER
-	        url     : false,        // FALSE or STRING - url to document to be used (async or not)
-	        json    : false,        // FALSE or OBJECT if type is JSON and async is false - the tree dump as json
-	        xml     : false         // FALSE or STRING
-	    },
-        ui      : {
-           dots        : true,     // BOOL - dots or no dots
-           rtl         : false,    // BOOL - is the tree right-to-left
-           animation   : 0,        // INT - duration of open/close animations in miliseconds
-           hover_mode  : true,     // SHOULD get_* functions chage focus or change hovered item
-           scroll_spd  : 4,
-           theme_path  : theme_path,    // Path to themes
-           theme_name  : "classic",// Name of theme
-        },
-
-        rules   : {
-           multiple    : false,    // FALSE | CTRL | ON - multiple selection off/ with or without holding Ctrl
-           metadata    : false,    // FALSE or STRING - attribute name (use metadata plugin)
-           type_attr   : "rel",    // STRING attribute name (where is the type stored if no metadata)
-           multitree   : false,    // BOOL - is drag n drop between trees allowed
-           createat    : "bottom", // STRING (top or bottom) new nodes get inserted at top or bottom
-           use_inline  : false,    // CHECK FOR INLINE RULES - REQUIRES METADATA
-           clickable   : "all",    // which node types can the user select | default - all
-           renameable  : false,    // which node types can the user select | default - all
-           deletable   : false,    // which node types can the user delete | default - all
-           creatable   : false,    // which node types can the user create in | default - all
-           draggable   : "none",   // which node types can the user move | default - none | "all"
-           dragrules   : "all",    // what move operations between nodes are allowed | default - none | "all"
-           drag_copy   : false,    // FALSE | CTRL | ON - drag to copy off/ with or without holding Ctrl
-           droppable   : [],
-           drag_button : "left"
-        },
-
-        callback    : {             // various callbacks to attach custom logic to
-           // before focus  - should return true | false
-           beforechange: function(NODE,TREE_OBJ) { return true },
-           beforeopen  : function(NODE,TREE_OBJ) { return true },
-           beforeclose : function(NODE,TREE_OBJ) { return true },
-           // before move   - should return true | false
-           beforemove  : function(NODE,REF_NODE,TYPE,TREE_OBJ) { return true }, 
-           // before create - should return true | false
-           beforecreate: function(NODE,REF_NODE,TYPE,TREE_OBJ) { return true }, 
-           // before rename - should return true | false
-           beforerename: function(NODE,LANG,TREE_OBJ) { return true }, 
-           // before delete - should return true | false
-           beforedelete: function(NODE,TREE_OBJ) { return true }, 
-
-           onJSONdata  : function(DATA,TREE_OBJ) { return DATA; },
-           onselect    : function(NODE,TREE_OBJ) {        	   
-               window.onerror = function(){return true;};
-        	   throw 'exit';
-        	},                  // node selected
-           ondeselect  : function(NODE,TREE_OBJ) { },                  // node deselected
-           onchange    : function(NODE,TREE_OBJ) { },                  // focus changed
-           onrename    : function(NODE,LANG,TREE_OBJ,RB) { },              // node renamed ISNEW - TRUE|FALSE, current language
-           onmove      : function(NODE,REF_NODE,TYPE,TREE_OBJ,RB) { }, // move completed (TYPE is BELOW|ABOVE|INSIDE)
-           oncopy      : function(NODE,REF_NODE,TYPE,TREE_OBJ,RB) { }, // copy completed (TYPE is BELOW|ABOVE|INSIDE)
-           oncreate    : function(NODE,REF_NODE,TYPE,TREE_OBJ,RB) { }, // node created, parent node (TYPE is createat)
-           ondelete    : function(NODE, TREE_OBJ,RB) { },                  // node deleted
-           onopen      : function(NODE, TREE_OBJ) { },                 // node opened
-           onopen_all  : function(TREE_OBJ) { },                       // all nodes opened
-           onclose     : function(NODE, TREE_OBJ) { },                 // node closed
-           error       : function(TEXT, TREE_OBJ) { },                 // error occured
-           // double click on node - defaults to open/close & select
-           ondblclk    : function(NODE, TREE_OBJ) { TREE_OBJ.toggle_branch.call(TREE_OBJ, NODE); TREE_OBJ.select_branch.call(TREE_OBJ, NODE); },
-           // right click - to prevent use: EV.preventDefault(); EV.stopPropagation(); return false
-           onrgtclk    : function(NODE, TREE_OBJ, EV) { },
-           onload      : function(TREE_OBJ) { },
-           onfocus     : function(TREE_OBJ) { },
-           ondrop      : function(NODE,REF_NODE,TYPE,TREE_OBJ) {}
-        }
-      });
    }
 }
--- theme/tripal_organism/tripal_organism_kegg_summary.tpl.php
+++ theme/tripal_organism/tripal_organism_kegg_summary.tpl.php
@@ -1,40 +1,23 @@
 <?php
 $organism = $node->organism;
 $form = $organism->tripal_analysis_kegg->select_form['form'];
-$has_results = $organism->tripal_analysis_kegg->select_form['has_results'];  
+$has_results = $organism->tripal_analysis_kegg->select_form['has_results'];
 
-if ($has_results) { ?>
-  <div id="tripal_organism-kegg_summary-box" class="tripal_organism-info-box tripal-info-box">
-    <div  class="tripal_organism-info-box-title tripal-info-box-title">KEGG Analysis Reports</div> 
-    <?php print drupal_render($form); ?>
-    <div id="tripal_analysis_kegg_org_report"></div>
-    <div id="tripal_ajaxLoading" style="display:none">
-      <div id="loadingText">Loading...</div>
-    </div>   
+if($has_results) {
+  print drupal_render($form); ?>
+  <div id="tripal_analysis_kegg_org_report"></div>
   </div> <?php
-} 
-else {
-  // show a message to the site administrator instructing how to enable 
-  // a KEGG report.  Otherwise, if the user is not an administrator and 
-  // there is no content then nothing get's shown.
-  if (user_access('access administration pages')) { ?>
-    <div id="tripal_organism-kegg_summary-box" class="tripal_organism-info-box tripal-info-box">
-      <div  class="tripal_organism-info-box-title tripal-info-box-title">KEGG Analysis Reports</div> 
-      <div class="tripal-no-results">
-        There are no KEGG reports available
-        <p><br>Administrators, to view a KEGG report you must:
-        <ul>
-          <li>Create a <a href="<?php print url('node/add/chado-analysis-kegg'); ?>" target="_blank">KEGG analysis page</a>.</li>
-          <li>Populate the <a href="<?php print url('admin/tripal/mviews'); ?>" target="_blank">kegg_by_organism</a> materialized view</li>
-          <li>Ensure the user <a href="<?php print url('admin/user/permissions'); ?>"> has permission</a> to view the KEGG analysis content</li>
-          <li>Refresh this page</li>
-        </ul> 
-        </p>
-        This page will not appear to site visitors unless the KEGG data is present. 
-      </div>
-    </div><?php 
-  }
 }
+  
+print tripal_set_message("Administrators, if there are no KEGG reports available, you must:
+    <ul>
+      <li>Create a <a href=\"" . url('node/add/chado-analysis-kegg') . "\" target=\"_blank\">KEGG analysis page</a>.</li>
+      <li>Populate the <a href=\"" . url('admin/tripal/schema/mviews'); . "\" target=\"_blank\">kegg_by_organism</a> materialized view</li>
+      <li>Ensure the user <a href=\"" . url('admin/people/permissions'). "\"> has permission</a> to view the KEGG analysis content</li>
+      <li>Refresh this page</li>
+    </ul> 
+    </p>
+    This page will not appear to site visitors unless the KEGG data is present.", TRIPAL_INFO, array('return_html' => 1)); 
 
 
 
