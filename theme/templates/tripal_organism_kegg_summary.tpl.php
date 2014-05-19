49546744269fc82b6ed4ef541ef4941246bce358
Stephen Ficklin <spficklin@gmail.com>
2014-05-19 11:31:24 -0400

    Fixed bugs in kegg summary template

--- theme/templates/tripal_organism_kegg_summary.tpl.php
+++ theme/templates/tripal_organism_kegg_summary.tpl.php
@@ -5,8 +5,7 @@ $has_results = $organism->tripal_analysis_kegg->select_form['has_results'];
 
 if($has_results) {
   print drupal_render($form); ?>
-  <div id="tripal_analysis_kegg_org_report"></div>
-  </div> <?php
+  <div id="tripal_analysis_kegg_org_report"></div> <?php
 }
   
 $message = 'Administrators, if there are no KEGG reports available, you must:
@@ -15,8 +14,6 @@ $message = 'Administrators, if there are no KEGG reports available, you must:
       <li>Populate the ' . l('kegg_by_organism','admin/tripal/schema/mviews', array('attributes' => array('tareget' => '_blank'))) . ' materialized view</li>
       <li>Ensure the user ' . l('has permission','admin/people/permissions', array('attributes' => array('tareget' => '_blank'))) . 'to view the KEGG analysis content</li>
       <li>Refresh this page</li>
-    </ul> 
-    </p>
-    This page will not appear to site visitors unless the KEGG data is present.';
+    </ul>';
 
 print tripal_set_message($message, TRIPAL_INFO, array('return_html' => 1)); 
