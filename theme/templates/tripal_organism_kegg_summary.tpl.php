845480ac261d97c1fd36ba6b05f1e8fbc626fdac
Stephen Ficklin <spficklin@gmail.com>
2014-09-04 15:00:31 -0400

    Tripal Analysis KEGG 7.x-2.0-rc (release candidate)

--- theme/templates/tripal_organism_kegg_summary.tpl.php
+++ theme/templates/tripal_organism_kegg_summary.tpl.php
@@ -10,9 +10,9 @@ if($has_results) {
   
 $message = 'Administrators, if there are no KEGG reports available, you must:
     <ul>
-      <li>Create a ' . l('KEGG analysis page', 'node/add/chado-analysis-kegg', array('attributes' => array('tareget' => '_blank'))) . '</li>
-      <li>Populate the ' . l('kegg_by_organism','admin/tripal/schema/mviews', array('attributes' => array('tareget' => '_blank'))) . ' materialized view</li>
-      <li>Ensure the user ' . l('has permission','admin/people/permissions', array('attributes' => array('tareget' => '_blank'))) . 'to view the KEGG analysis content</li>
+      <li>Create a ' . l('KEGG analysis page', 'node/add/chado-analysis-kegg', array('attributes' => array('target' => '_blank'))) . '</li>
+      <li>Populate the ' . l('kegg_by_organism','admin/tripal/schema/mviews', array('attributes' => array('target' => '_blank'))) . ' materialized view</li>
+      <li>Ensure the user ' . l('has permission','admin/people/permissions', array('attributes' => array('target' => '_blank'))) . 'to view the KEGG analysis content</li>
       <li>Refresh this page</li>
     </ul>';
 
